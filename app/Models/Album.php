76a019a3cacbeb944025e77c317da20a1f883704
Viktor <norad-2013@mail.ru>
2018-03-07 23:08:18 +0700

    модель альбома

---
+++ app/Models/Album.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by Viktor Leukhin.
+ * Tel: +7-926-797-5419
+ * E-mail: vsleuhin@ya.ru
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Album extends Model
+{
+    protected $table = 'albums';
+
+    protected $casts = [
+        'photos' => 'array'
+    ];
+}
